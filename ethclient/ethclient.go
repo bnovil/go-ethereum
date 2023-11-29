5c85c452fd95497b8ad372cd953db985e0dfcb47
bnovil <lzqcn2000@126.com>
2023-11-29 15:41:41 +0800

    fix syncing response

--- ethclient/ethclient.go
+++ ethclient/ethclient.go
@@ -669,25 +669,25 @@ func toCallArg(msg ethereum.CallMsg) interface{} {
 
 // rpcProgress is a copy of SyncProgress with hex-encoded fields.
 type rpcProgress struct {
-	StartingBlock hexutil.Uint64
-	CurrentBlock  hexutil.Uint64
-	HighestBlock  hexutil.Uint64
+	startingBlock hexutil.Uint64
+	currentBlock  hexutil.Uint64
+	highestBlock  hexutil.Uint64
 
-	PulledStates hexutil.Uint64
-	KnownStates  hexutil.Uint64
+	pulledStates hexutil.Uint64
+	knownStates  hexutil.Uint64
 
-	SyncedAccounts      hexutil.Uint64
-	SyncedAccountBytes  hexutil.Uint64
-	SyncedBytecodes     hexutil.Uint64
-	SyncedBytecodeBytes hexutil.Uint64
-	SyncedStorage       hexutil.Uint64
-	SyncedStorageBytes  hexutil.Uint64
-	HealedTrienodes     hexutil.Uint64
-	HealedTrienodeBytes hexutil.Uint64
-	HealedBytecodes     hexutil.Uint64
-	HealedBytecodeBytes hexutil.Uint64
-	HealingTrienodes    hexutil.Uint64
-	HealingBytecode     hexutil.Uint64
+	syncedAccounts      hexutil.Uint64
+	syncedAccountBytes  hexutil.Uint64
+	syncedBytecodes     hexutil.Uint64
+	syncedBytecodeBytes hexutil.Uint64
+	syncedStorage       hexutil.Uint64
+	syncedStorageBytes  hexutil.Uint64
+	healedTrienodes     hexutil.Uint64
+	healedTrienodeBytes hexutil.Uint64
+	healedBytecodes     hexutil.Uint64
+	healedBytecodeBytes hexutil.Uint64
+	healingTrienodes    hexutil.Uint64
+	healingBytecode     hexutil.Uint64
 }
 
 func (p *rpcProgress) toSyncProgress() *ethereum.SyncProgress {
@@ -695,22 +695,22 @@ func (p *rpcProgress) toSyncProgress() *ethereum.SyncProgress {
 		return nil
 	}
 	return &ethereum.SyncProgress{
-		StartingBlock:       uint64(p.StartingBlock),
-		CurrentBlock:        uint64(p.CurrentBlock),
-		HighestBlock:        uint64(p.HighestBlock),
-		PulledStates:        uint64(p.PulledStates),
-		KnownStates:         uint64(p.KnownStates),
-		SyncedAccounts:      uint64(p.SyncedAccounts),
-		SyncedAccountBytes:  uint64(p.SyncedAccountBytes),
-		SyncedBytecodes:     uint64(p.SyncedBytecodes),
-		SyncedBytecodeBytes: uint64(p.SyncedBytecodeBytes),
-		SyncedStorage:       uint64(p.SyncedStorage),
-		SyncedStorageBytes:  uint64(p.SyncedStorageBytes),
-		HealedTrienodes:     uint64(p.HealedTrienodes),
-		HealedTrienodeBytes: uint64(p.HealedTrienodeBytes),
-		HealedBytecodes:     uint64(p.HealedBytecodes),
-		HealedBytecodeBytes: uint64(p.HealedBytecodeBytes),
-		HealingTrienodes:    uint64(p.HealingTrienodes),
-		HealingBytecode:     uint64(p.HealingBytecode),
+		StartingBlock:       uint64(p.startingBlock),
+		CurrentBlock:        uint64(p.currentBlock),
+		HighestBlock:        uint64(p.highestBlock),
+		PulledStates:        uint64(p.pulledStates),
+		KnownStates:         uint64(p.knownStates),
+		SyncedAccounts:      uint64(p.syncedAccounts),
+		SyncedAccountBytes:  uint64(p.syncedAccountBytes),
+		SyncedBytecodes:     uint64(p.syncedBytecodes),
+		SyncedBytecodeBytes: uint64(p.syncedBytecodeBytes),
+		SyncedStorage:       uint64(p.syncedStorage),
+		SyncedStorageBytes:  uint64(p.syncedStorageBytes),
+		HealedTrienodes:     uint64(p.healedTrienodes),
+		HealedTrienodeBytes: uint64(p.healedTrienodeBytes),
+		HealedBytecodes:     uint64(p.healedBytecodes),
+		HealedBytecodeBytes: uint64(p.healedBytecodeBytes),
+		HealingTrienodes:    uint64(p.healingTrienodes),
+		HealingBytecode:     uint64(p.healingBytecode),
 	}
 }
